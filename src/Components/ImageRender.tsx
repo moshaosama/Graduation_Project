bebaca49dc8fb5ed5cdfebc4b8576edfcf59fdb8
moshaosama <mohamedOSFekry@gmail.com>
2025-04-30 13:46:38 +0300

    update: ImageRender component

--- src/Components/ImageRender.tsx
+++ src/Components/ImageRender.tsx
@@ -2,7 +2,7 @@ import React, { useMemo } from "react";
 
 const ImageRender = React.memo(
   ({ src, alt, width }: { src: string; alt: string; width: string }) => {
-    const computedClass = useMemo(() => `w-${width}`, [width]);
+    const computedClass = useMemo(() => `w-${width} max-sm:w-12`, [width]);
 
     return <img src={src} alt={alt} loading="lazy" className={computedClass} />;
   }
