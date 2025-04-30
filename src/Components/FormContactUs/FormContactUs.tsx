8a72ed2af2a683674d7e72ec5f5c7efa6dab440b
moshaosama <mohamedOSFekry@gmail.com>
2025-04-30 13:46:37 +0300

    update: FormContactUs component

--- src/Components/FormContactUs/FormContactUs.tsx
+++ src/Components/FormContactUs/FormContactUs.tsx
@@ -7,12 +7,14 @@ import FormContectusList from "./FormContectusList";
 
 const FormContactUs = () => {
   return (
-    <div className="border-[3px] border-solid m-10 h-fit mx-36 p-[2.4pc] rounded-md shadow-2xl border-gray-500">
+    <div className="border-[3px] border-solid m-10 h-fit mx-36 max-sm:-mx-4 max-sm:w-[24pc] p-[2.4pc] rounded-md shadow-2xl border-gray-500">
       <h1 className="text-3xl font-bold text-[#343437] flex justify-center">
         Contact US
       </h1>
-      <div className="flex items-center mt-10 mx-5">
-        <FormContectusList />
+      <div className="flex items-center mt-10 mx-5 max-sm:mx-0 max-sm:flex-col">
+        <div className="mr-10 max-sm:ml-10 max-sm:mb-10">
+          <FormContectusList />
+        </div>
         <div className="flex flex-col gap-7">
           <div className="flex items-center gap-3">
             <CiLocationOn className="text-xl" />
