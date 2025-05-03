b44f2f96004101f84127f08e4828e0b394cda9f7
moshaosama <mohamedOSFekry@gmail.com>
2025-05-04 01:15:15 +0300

    refactor: update JoinNewDoctor page to use new structure

--- src/Pages/JoinNewDoctor.tsx
+++ src/Pages/JoinNewDoctor.tsx
@@ -1,9 +1,19 @@
-import JoinNewDoctorForm from "../Components/JoinNewDoctor/JoinNewDoctorForm";
+import JoinNewDoctorForm from "../Components/JoinNewDoctor/Components/JoinNewDoctorForm";
+import PersonalInfo from "../Components/JoinNewDoctor/Components/PersonalInfo";
+import { usePersonalInfoContext } from "../Components/JoinNewDoctor/Context/PersonalInfoContext";
 
 const JoinNewDoctor = () => {
+  const { OpenModelPersonalInfo, toogleOpenModelPersonalInfo } =
+    usePersonalInfoContext();
   return (
     <>
-      <JoinNewDoctorForm />
+      {OpenModelPersonalInfo ? (
+        <JoinNewDoctorForm
+          handleContinueWithEmail={toogleOpenModelPersonalInfo}
+        />
+      ) : null}
+      {OpenModelPersonalInfo ? null : <PersonalInfo />}
+      {/* {OpenModelPersonalInfo ? null : <LoginFormAsDoctor />} */}
     </>
   );
 };
