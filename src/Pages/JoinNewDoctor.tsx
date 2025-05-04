5c94c133b2328d6a0ecfa618974b86ef4aacb7cf
moshaosama <mohamedOSFekry@gmail.com>
2025-05-04 14:19:55 +0300

    Update JoinNewDoctor page to include new gender logic

--- src/Pages/JoinNewDoctor.tsx
+++ src/Pages/JoinNewDoctor.tsx
@@ -1,3 +1,4 @@
+import {  useNavigate } from "react-router";
 import JoinNewDoctorForm from "../Components/JoinNewDoctor/Components/JoinNewDoctorForm";
 import MainComponent from "../Components/JoinNewDoctor/Components/MainComponent";
 import PersonalInfo from "../Components/JoinNewDoctor/Components/PersonalInfo";
@@ -6,17 +7,28 @@ import { usePersonalInfoContext } from "../Components/JoinNewDoctor/Context/Pers
 const JoinNewDoctor = () => {
   const { OpenModelPersonalInfo, toogleOpenModelPersonalInfo } =
     usePersonalInfoContext();
+  const Navigate = useNavigate();
+
+  const handleClickBack = () => {
+    Navigate("/");
+  };
   return (
     <>
       {OpenModelPersonalInfo ? (
-        <MainComponent Title="Create a new account">
+        <MainComponent
+          Title="Create a new account"
+          handleClickBack={handleClickBack}
+        >
           <JoinNewDoctorForm
             handleContinueWithEmail={toogleOpenModelPersonalInfo}
           />
         </MainComponent>
       ) : null}
       {OpenModelPersonalInfo ? null : (
-        <MainComponent Title="Enter your personal info">
+        <MainComponent
+          Title="Enter your personal info"
+          handleClickBack={toogleOpenModelPersonalInfo}
+        >
           <PersonalInfo />
         </MainComponent>
       )}
