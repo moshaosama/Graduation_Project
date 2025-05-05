636b1127d372a6bcaefb4591270c38e9fbff384b
moshaosama <mohamedOSFekry@gmail.com>
2025-05-06 00:53:26 +0300

    Latest version

--- src/Components/ProfileDoctorDoctorComponent/DetailsProfileDoctor/DetailsProfileDoctor.tsx
+++ src/Components/ProfileDoctorDoctorComponent/DetailsProfileDoctor/DetailsProfileDoctor.tsx
@@ -28,6 +28,11 @@ const DetailsProfileDoctor = () => {
 
   const { OpenModel, handleOpenModel } = useFormTransition();
 
+  const { handleSubmit, register } = useForm();
+  const handleFormSubmit = (data: any) => {
+    console.log(data);
+  };
+
   if (!Doctor) {
     return (
       <div className="w-full text-center py-20">
@@ -38,31 +43,29 @@ const DetailsProfileDoctor = () => {
     );
   }
 
-  const { handleSubmit, register } = useForm();
-  const handleFormSubmit = (data: any) => {
-    console.log(data);
-  };
-
   return (
     <form onSubmit={handleSubmit(handleFormSubmit)}>
-      <div className="w-full">
-        <div className="text-gray-500 font-semibold flex items-center max-sm:w-fit gap-2">
-          <h1 className="underline cursor-pointer">Home</h1> {" > "}
+      <div className="w-full px-6 md:px-10">
+        <div className="text-gray-500 font-semibold flex items-center gap-2">
+          <h1 className="underline cursor-pointer">Home</h1> {">"}
           <h1 className="underline cursor-pointer">Find a specialist</h1>
         </div>
-        <div className="my-10 flex gap-5 w-fit">
+
+        <div className="my-10 flex flex-col md:flex-row gap-5">
           <img
             src={Doctor?.Gender === "Male" ? ImageMale : ImageFemale}
             alt="ImageDoctor.png"
             loading="lazy"
             decoding="async"
-            className="w-52 object-cover rounded"
+            className="w-52 object-cover rounded self-center"
           />
-          <div className="flex justify-between flex-col">
-            <div className="flex flex-col gap-3">
-              <div className="flex items-center gap-10">
+
+          <div className="flex flex-col justify-between gap-5">
+            <div className="space-y-4">
+              {/* Name Field */}
+              <div className="flex items-center gap-4">
                 <h1 className="text-blue-700 text-xl font-bold flex items-center gap-4">
-                  {Doctor.Name}{" "}
+                  {Doctor.Name}
                   {User?.DoctorId === Doctor.DoctorID && (
                     <MdOutlineEdit
                       className="text-gray-500 text-xl cursor-pointer"
@@ -79,38 +82,40 @@ const DetailsProfileDoctor = () => {
                   {...register("Name")}
                 />
               </div>
-              <div className="flex items-center">
+
+              {/* Specialty Field */}
+              <div className="flex items-center gap-4">
                 <MdOutlineHealthAndSafety className="text-2xl text-blue-400" />
-                <div className="flex items-center gap-10">
-                  <h1 className="font-semibold flex items-center gap-4">
-                    {Doctor.Specialty_name}
-                    {User?.DoctorId === Doctor.DoctorID && (
-                      <MdOutlineEdit
-                        className="text-gray-500 text-xl cursor-pointer"
-                        onClick={() =>
-                          handleOpenModel(OpenModelTypes.Speciality)
-                        }
-                      />
-                    )}
-                  </h1>
-                  <input
-                    type="text"
-                    className={clsx(
-                      "rounded-lg transition-all duration-300 border-black",
-                      OpenModel.Speciality ? "w-96 border-2 p-1" : "w-0"
-                    )}
-                    {...register("Specialty_name")}
-                  />
-                </div>
+                <h1 className="font-semibold flex items-center gap-4">
+                  {Doctor.Specialty_name}
+                  {User?.DoctorId === Doctor.DoctorID && (
+                    <MdOutlineEdit
+                      className="text-gray-500 text-xl cursor-pointer"
+                      onClick={() => handleOpenModel(OpenModelTypes.Speciality)}
+                    />
+                  )}
+                </h1>
+                <input
+                  type="text"
+                  className={clsx(
+                    "rounded-lg transition-all duration-300 border-black",
+                    OpenModel.Speciality ? "w-96 border-2 p-1" : "w-0"
+                  )}
+                  {...register("Specialty_name")}
+                />
               </div>
-              <p className="text-gray-700 font-medium max-sm:w-20 flex items-center gap-4">
-                {Doctor?.Location || "no Location"}
-                {User?.DoctorId === Doctor.DoctorID && (
-                  <MdOutlineEdit
-                    className="text-gray-500 text-xl cursor-pointer"
-                    onClick={() => handleOpenModel(OpenModelTypes.Location)}
-                  />
-                )}
+
+              {/* Location Field */}
+              <div className="flex items-center gap-4">
+                <p className="text-gray-700 font-medium">
+                  {Doctor?.Location || "No Location"}
+                  {User?.DoctorId === Doctor.DoctorID && (
+                    <MdOutlineEdit
+                      className="text-gray-500 text-xl cursor-pointer ml-4"
+                      onClick={() => handleOpenModel(OpenModelTypes.Location)}
+                    />
+                  )}
+                </p>
                 <input
                   type="text"
                   className={clsx(
@@ -119,14 +124,15 @@ const DetailsProfileDoctor = () => {
                   )}
                   {...register("Location")}
                 />
-              </p>
-              <p className="flex gap-2">
-                Fees:
-                <span className="font-bold flex items-center gap-4">
-                  {Doctor?.ConsultationFee} EGB
+              </div>
+
+              {/* Fees Field */}
+              <div className="flex items-center gap-4">
+                <span className="font-bold">
+                  Fees: {Doctor?.ConsultationFee} EGB
                   {User?.DoctorId === Doctor.DoctorID && (
                     <MdOutlineEdit
-                      className="text-gray-500 text-xl cursor-pointer"
+                      className="text-gray-500 text-xl cursor-pointer ml-4"
                       onClick={() => handleOpenModel(OpenModelTypes.Fees)}
                     />
                   )}
@@ -139,41 +145,46 @@ const DetailsProfileDoctor = () => {
                   )}
                   {...register("ConsultationFee")}
                 />
-              </p>
+              </div>
             </div>
-            <div className="border-[1px] font-semibold hover:bg-red-500 hover:text-black cursor-pointer transition-all duration-300 border-red-500 border-solid w-fit px-3 py-1 rounded-full">
+
+            <div className="border font-semibold hover:bg-red-500 hover:text-black cursor-pointer transition-all duration-300 border-red-500 w-fit px-3 py-1 rounded-full">
               <p>01015151515</p>
             </div>
           </div>
         </div>
-        <div className="justify-between flex max-sm:flex-col w-full mx-56 max-sm:mx-0 ">
-          <div className="flex gap-4 items-center w-fit">
-            <div className="flex items-center">
-              <img
-                src="/1_k5m8q1rRpdQ-H5NogaHnBQ-removebg-preview.png"
-                alt="Rating.png"
-                loading="lazy"
-                decoding="async"
-                className="w-24"
-              />
-              <h1 className="text-blue-500 font-bold">4.0 (6 ratings)</h1>
-            </div>
+
+        <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+          <div className="flex gap-4 items-center">
+            <img
+              src="/1_k5m8q1rRpdQ-H5NogaHnBQ-removebg-preview.png"
+              alt="Rating.png"
+              loading="lazy"
+              decoding="async"
+              className="w-24"
+            />
+            <h1 className="text-blue-500 font-bold">4.0 (6 ratings)</h1>
             <a
               href="#leaveReview"
               className="text-blue-500 font-bold hover:underline"
             >
-              Leave a review{" "}
+              Leave a review
             </a>
           </div>
-          <div className="flex gap-2">
-            <div className="bg-red-600 max-sm:w-fit max-sm:mt-2 text-white cursor-pointer font-semibold hover:bg-red-400 transition-all duration-500  px-5 py-2 rounded-xl shadow-lg">
-              <h1>Book Tomorrow  (01:00 PM - 04:00 PM)</h1>
+
+          <div className="flex flex-col sm:flex-row gap-2 items-center">
+            <div className="bg-red-600 text-white cursor-pointer font-semibold hover:bg-red-400 transition-all duration-500 px-5 py-2 rounded-xl shadow-lg">
+              <h1>Book Tomorrow (01:00 PM - 04:00 PM)</h1>
             </div>
-            <button className="bg-blue-500 px-5 font-bold hover:bg-blue-400 transition-all duration-200 rounded-lg text-white">
+            <button
+              type="submit"
+              className="bg-blue-500 px-5 font-bold hover:bg-blue-400 transition-all duration-200 rounded-lg text-white"
+            >
               Save Changes
             </button>
           </div>
         </div>
+
         <hr className="my-10 h-1" />
       </div>
     </form>
