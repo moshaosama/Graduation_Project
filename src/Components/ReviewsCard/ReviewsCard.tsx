d4ee645241de7445674b0b0e2e7c0cd5427b8d66
moshaosama <mohamedOSFekry@gmail.com>
2025-05-06 00:12:47 +0300

    Updated ReviewsCard component

--- src/Components/ReviewsCard/ReviewsCard.tsx
+++ src/Components/ReviewsCard/ReviewsCard.tsx
@@ -17,35 +17,39 @@ const ReviewsCard = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    dispatch(fetchgetReviews(+id!));
+    if (id) {
+      dispatch(fetchgetReviews(+id));
+    }
   }, [id, dispatch]);
 
   return (
     <>
-      {id
-        ? state?.data?.result?.map((el: ReviewCartType) => {
-            return (
-              <div
-                className="w-full max-sm:w-96 mt-5 border-[3px] p-4 border-solid"
-                key={el.id}
-              >
-                <img
-                  src="/1_k5m8q1rRpdQ-H5NogaHnBQ-removebg-preview.png"
-                  alt="Rating.png"
-                  loading="lazy"
-                  decoding="async"
-                  className="w-28"
-                />
-                <p className="mx-2 ">{el.title}</p>
-                <div className="flex justify-end">
-                  <p className="text-gray-400 font-bold">
-                    {el.userName} - {el.Date}
-                  </p>
-                </div>
-              </div>
-            );
-          })
-        : null}
+      {state.loading && <p>Loading...</p>}
+      {state.error && <p>{state.error}</p>}
+      {state?.data?.result?.length > 0 ? (
+        state.data.result.map((el: ReviewCartType) => (
+          <div
+            className="w-full max-sm:w-96 mt-5 border-[3px] p-4 border-solid"
+            key={el.id}
+          >
+            <img
+              src="/1_k5m8q1rRpdQ-H5NogaHnBQ-removebg-preview.png"
+              alt="Rating.png"
+              loading="lazy"
+              decoding="async"
+              className="w-28"
+            />
+            <p className="mx-2 ">{el.title}</p>
+            <div className="flex justify-end">
+              <p className="text-gray-400 font-bold">
+                {el.userName} - {el.Date}
+              </p>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>No reviews available.</p>
+      )}
     </>
   );
 };
