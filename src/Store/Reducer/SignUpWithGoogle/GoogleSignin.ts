27f13194247a27ba2095e42eac814951ab14ce1e
moshaosama <mohamedOSFekry@gmail.com>
2025-05-06 20:16:29 +0300

    Added SignUpWithGoogle reducer for handling Google auth

---
+++ src/Store/Reducer/SignUpWithGoogle/GoogleSignin.ts
@@ -0,0 +1,43 @@
+import { ReduxData } from "../../../Types/StoreData/ReduxData";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState: ReduxData = {
+  data: {},
+  loading: false,
+  error: "",
+};
+
+export const fetchSignInWithGoogle = createAsyncThunk(
+  "GoogleOAuth/fetchSignInWithGoogle",
+  () => {
+    fetch("http://localhost:3000/auth/google")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        return data;
+      });
+  }
+);
+
+const SignInGoogle = createSlice({
+  name: "GoogleOAuth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSignInWithGoogle.pending, (state) => {
+        state.loading = true;
+        state.error = "";
+      })
+      .addCase(fetchSignInWithGoogle.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchSignInWithGoogle.rejected, (state) => {
+        state.loading = false;
+      });
+  },
+});
+
+export default SignInGoogle.reducer;
