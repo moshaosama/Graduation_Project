6fe8cf1c456262146528100ba831654488a1d33c
moshaosama <mohamedOSFekry@gmail.com>
2025-05-06 20:16:29 +0300

    Modified Navbar types to match latest updates

--- src/Types/Navbar.tsx
+++ src/Types/Navbar.tsx
@@ -2,7 +2,15 @@ export interface NavbarProps {
   setFun: () => void;
 }
 export interface User {
+  displayName: string;
   DoctorId: number;
   userName: string;
   Status: string;
+  Email: string;
+  emails: {
+    value: string;
+  }[];
+  photos: {
+    value: string;
+  }[];
 }
