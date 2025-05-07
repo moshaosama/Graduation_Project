ff57a6e782514e3ab39432af1464d1043328ab01
moshaosama <mohamedOSFekry@gmail.com>
2025-05-07 12:12:36 +0300

    إضافة ملف CopyRight.tsx

---
+++ src/Components/CopyRight.tsx
@@ -0,0 +1,16 @@
+const CopyRight = () => {
+  return (
+    <div className="flex justify-between max-sm:flex-col-reverse max-sm:gap-5 mx-80 max-sm:mx-5 my-5">
+      <div>
+        <h1 className="font-bold">© 2025 MedicalPulse. All rights reserved.</h1>
+      </div>
+      <div className="flex gap-10 max-sm:flex-col max-sm:gap-2">
+        <h1 className="underline font-bold">Privacy Policy</h1>
+        <h1 className="underline font-bold">Terms of Service</h1>
+        <h1 className="underline font-bold">Cookie Settings</h1>
+      </div>
+    </div>
+  );
+};
+
+export default CopyRight;
