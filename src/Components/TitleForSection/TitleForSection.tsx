0fbcc7929b860dde58c9c3efdc521ebd85b176b7
moshaosama <mohamedOSFekry@gmail.com>
2025-05-07 15:40:17 +0300

    Refactor TitleForSection component layout

--- src/Components/TitleForSection/TitleForSection.tsx
+++ src/Components/TitleForSection/TitleForSection.tsx
@@ -5,7 +5,7 @@ const TitleForSection = React.memo(({ Text }: { Text: string }) => {
   const { language } = useConvertLanguage();
   return (
     <h1
-      className={`text-[#184C99] max-sm:w-fit text-5xl max-sm:text-3xl font-bold ${
+      className={`text-[#184C99] max-sm:w-fit text-4xl max-sm:text-3xl font-bold ${
         language === "English" ? "text-end" : ""
       } `}
     >
