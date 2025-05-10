d2a3a408003713813e1daaa7b354f8a51ea711f4
moshaosama <mohamedOSFekry@gmail.com>
2025-05-10 21:34:02 +0300

    Add LivePhoto component

---
+++ src/features/About Us/Components/LivePhoto.tsx
@@ -0,0 +1,28 @@
+import clsx from "clsx";
+import useScrollTrigger from "../../../Hooks/useScrollTrigger";
+
+const LivePhoto = () => {
+  const scrolled = useScrollTrigger(12);
+  return (
+    <div
+      className={clsx(
+        "mt-10 flex justify-center w-[170dvh] max-sm:w-fit items-start max-sm:mx-24 transition-all duration-500",
+        scrolled ? "opacity-100 gap-7" : "opacity-0 gap-96"
+      )}
+    >
+      <div className="relative flex mt-20">
+        <div className="bg-[#1f40c36b] rounded-xl w-60 max-sm:w-48 h-60 max-sm:h-48"></div>
+        <div className="bg-[#1f40c36b] rounded-xl absolute  right-20 top-20 max-sm:w-48 max-sm:h-48 w-60 h-60"></div>
+        <img
+          src="public\Ellipse 17.png"
+          alt="logo.png"
+          className="w-36 max-sm:w-32 h-36 max-sm:h-32 absolute right-20 top-20"
+        />
+        <div className="border-[7px]  rounded-full border-dotted border-[#6893E4] w-52 max-sm:w-40 h-52 max-sm:h-40 absolute right-14 max-sm:right-16 top-14 max-sm:top-14  animate-spin-slow"></div>
+        <div className="border-[7px]  rounded-full border-dotted border-[#6893E4] w-64 max-sm:w-56 h-64 max-sm:h-56 absolute top-8 max-sm:top-6 right-8 max-sm:right-7 animate-spin-slow"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LivePhoto;
