3c6f5f80c83ea1b6cedeedbedd021f232116408d
moshaosama <mohamedOSFekry@gmail.com>
2025-05-10 20:28:37 +0300

    Create AboutUs page

---
+++ src/Pages/AboutUs.tsx
@@ -0,0 +1,19 @@
+import JoinUs from "../Components/JoinUs/JoinUs";
+import HeaderAboutUs from "../features/About Us/Components/Header";
+import OurStory from "../features/About Us/Components/OurStory";
+import OurVision from "../features/About Us/Components/OurVision";
+const AboutUs = () => {
+  return (
+    <>
+      <HeaderAboutUs/>
+      <OurStory/>
+      <OurVision/>
+      <JoinUs/>
+    </>
+
+  );
+};
+
+export default AboutUs;
+
+
