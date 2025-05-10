17baac3aa48aadbc0570561b53064323fc2ae290
moshaosama <mohamedOSFekry@gmail.com>
2025-05-10 21:34:05 +0300

    Add OurVisionData source file

---
+++ src/features/About Us/Data/OurVisionData.tsx
@@ -0,0 +1,20 @@
+export const OurVisionData = [
+    {
+        id: 1,
+        icon: "🌍",
+        title: "Impact",
+        description: "To empower users across communities with quick, reliable medical services they can access anytime, anywhere."
+    },
+    {
+        id: 2,
+        icon: "💡",
+        title: "Innovation",
+        description: "We aim to integrate AI, modern web technologies, and a secure database system to transform how people approach self-diagnosis and find care."
+    },
+    {
+        id: 3,
+        icon: "🔮",
+        title: "Innovation",
+        description: "MediPulse is more than a project—it's the start of a bigger mission. We plan to expand into mobile apps, multilingual support, and real medical partnerships."
+    }
+]
