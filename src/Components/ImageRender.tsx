712cce31a74681f1a14a53c0dd15fceb2fc33ee5
moshaosama <mohamedOSFekry@gmail.com>
2025-05-10 20:28:27 +0300

    Modify ImageRender component

--- src/Components/ImageRender.tsx
+++ src/Components/ImageRender.tsx
@@ -1,10 +1,11 @@
+import clsx from "clsx";
 import React, { useMemo } from "react";
 
 const ImageRender = React.memo(
   ({ src, alt, width }: { src: string; alt: string; width: string }) => {
     const computedClass = useMemo(() => `w-${width} max-sm:w-12`, [width]);
 
-    return <img src={src} alt={alt} loading="lazy" className={computedClass} />;
+    return <img src={src} alt={alt} loading="lazy" className={clsx(computedClass, "object-cover")} />;
   }
 );
 
