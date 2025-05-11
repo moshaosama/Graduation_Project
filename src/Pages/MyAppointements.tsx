a29de99088d0dc68c43ec701e6685998293e0c23
moshaosama <mohamedOSFekry@gmail.com>
2025-05-12 02:36:59 +0300

    Add MyAppointements page

---
+++ src/Pages/MyAppointements.tsx
@@ -0,0 +1,13 @@
+import GetAppointments from "../features/MyAppointments/Components/getAppointments";
+import Header from "../features/MyAppointments/Components/Header";
+
+const MyAppointements = () => {
+  return (
+    <>
+      <Header />
+      <GetAppointments />
+    </>
+  );
+};
+
+export default MyAppointements;
