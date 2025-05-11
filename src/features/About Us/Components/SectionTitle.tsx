d586eb7a983e0b38f41244a9dc3aafa8fa59f340
moshaosama <mohamedOSFekry@gmail.com>
2025-05-11 12:41:49 +0300

    Add SectionTitle component for About Us section

---
+++ src/features/About Us/Components/SectionTitle.tsx
@@ -0,0 +1,14 @@
+const SectionTitle = () => {
+  return (
+    <div className="w-full py-10 my-32 bg-[#1f3fc0b9] flex justify-center items-center">
+      <h1
+        className="text-white text-4xl font-bold font-playwrite"
+        style={{ fontOpticalSizing: "auto" }}
+      >
+        Medipulse
+      </h1>
+    </div>
+  );
+};
+
+export default SectionTitle;
