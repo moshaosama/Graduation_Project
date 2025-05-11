7c266347bd203f104070dd892125048dc3fc47b2
moshaosama <mohamedOSFekry@gmail.com>
2025-05-12 01:21:09 +0300

    Add BookDoctor page

---
+++ src/Pages/BookDoctor.tsx
@@ -0,0 +1,12 @@
+import Header from "../features/BookDoctor/Components/Header";
+import FormBookDoctor from "../features/BookDoctor/Components/FormBookDoctor";
+const BookDoctor = () => {
+  return (
+    <>
+      <Header />
+      <FormBookDoctor />
+    </>
+  );
+};
+
+export default BookDoctor;
