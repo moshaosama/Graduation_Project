0491e2f186b1d86574a22db0c2ca34ab6a5d5785
moshaosama <mohamedOSFekry@gmail.com>
2025-05-11 12:41:43 +0300

    Update AboutUs page with new content or layout

--- src/Pages/AboutUs.tsx
+++ src/Pages/AboutUs.tsx
@@ -2,18 +2,17 @@ import JoinUs from "../Components/JoinUs/JoinUs";
 import HeaderAboutUs from "../features/About Us/Components/Header";
 import OurStory from "../features/About Us/Components/OurStory";
 import OurVision from "../features/About Us/Components/OurVision";
+import SectionTitle from "../features/About Us/Components/SectionTitle";
 const AboutUs = () => {
   return (
     <>
-      <HeaderAboutUs/>
-      <OurStory/>
-      <OurVision/>
-      <JoinUs/>
+      <HeaderAboutUs />
+      <OurStory />
+      <OurVision />
+      <SectionTitle />
+      <JoinUs />
     </>
-
   );
 };
 
 export default AboutUs;
-
-
