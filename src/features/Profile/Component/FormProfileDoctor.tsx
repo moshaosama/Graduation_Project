5c52e6a704920c5ac1ac3822452e063376f08227
moshaosama <mohamedOSFekry@gmail.com>
2025-05-12 14:47:51 +0300

    Update FormProfileDoctor component layout and form handling

--- src/features/Profile/Component/FormProfileDoctor.tsx
+++ src/features/Profile/Component/FormProfileDoctor.tsx
@@ -6,42 +6,42 @@ const FormProfileDoctor = () => {
 
   return (
     <div>
-      <h1 className="text-3xl mx-auto font-bold text-[#184C99]">
+      <h1 className="text-3xl max-sm:text-center mx-auto font-bold text-[#184C99]">
         Manage Profile
       </h1>
-      <form className="grid grid-cols-2 gap-4 mt-10 w-full">
+      <form className="grid grid-cols-2 gap-4 mt-10 w-full max-sm:mx-4">
         <div className="col-span-2">
           <input
             type="text"
-            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[23pc]"
+            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[24pc]"
             value={User.userName}
           />
         </div>
         <div className="col-span-2">
           <input
             type="text"
-            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[23pc]"
+            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[24pc]"
             value={User.mobilePhone}
           />
         </div>
         <div className="col-span-2">
           <input
             type="text"
-            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[23pc]"
+            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[24pc]"
             value={Doctor.data.result?.[0].Specialty_name}
           />
         </div>
         <div className="col-span-2">
           <input
             type="text"
-            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[23pc]"
+            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[24pc]"
             value={User.Email}
           />
         </div>
         <div className="col-span-2">
           <input
             type="text"
-            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[23pc]"
+            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[24pc]"
             value={
               Doctor.data.result?.[0].ConsultationFee
                 ? Doctor.data.result?.[0].ConsultationFee.toString()
@@ -52,7 +52,7 @@ const FormProfileDoctor = () => {
         <div className="col-span-2">
           <input
             type="text"
-            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[23pc]"
+            className="border-2 border-gray-300 rounded-md p-2 w-[40pc] max-sm:w-[24pc]"
             value={
               Doctor.data.result?.[0].Clinic
                 ? Doctor.data.result?.[0].Clinic
@@ -60,12 +60,12 @@ const FormProfileDoctor = () => {
             }
           />
         </div>
-        <div className="flex gap-5">
-          <button className="bg-red-500 text-white px-4 py-2 rounded-md w-52 hover:bg-red-600 transition-all duration-200">
+        <div className="col-span-2 flex gap-5">
+          <button className="bg-red-500 text-white  py-2 rounded-md w-52 max-sm:w-32 hover:bg-red-600 transition-all duration-200">
             Save
           </button>
           <Link to={"/"}>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md w-52 hover:bg-blue-600 transition-all duration-200">
+            <button className="bg-blue-500 text-white py-2 rounded-md w-52 max-sm:w-32 hover:bg-blue-600 transition-all duration-200">
               Cancel
             </button>
           </Link>
