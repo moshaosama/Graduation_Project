48e7cae78c73b700b80a60b56deea1c0d6aef0a1
moshaosama <mohamedOSFekry@gmail.com>
2025-05-17 17:14:19 +0300

    Improve useFormData hook for better form handling

--- src/features/PopUpChatBot/Hooks/useFormData.ts
+++ src/features/PopUpChatBot/Hooks/useFormData.ts
@@ -2,7 +2,6 @@ import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../../Store/Store";
 import { fetchSendMessage } from "../Actions/SendMessage";
-import { fetchStartChat } from "../Actions/StartChat";
 const useFormData = () => {
   const {
     register,
@@ -13,19 +12,9 @@ const useFormData = () => {
 
   const onSubmit = async (data: any) => {
     try {
-      await dispatch(fetchStartChat("en")).unwrap();
-      const ChatBot: { session_id: string } = JSON.parse(
-        localStorage.getItem("SessionID") || "{}"
-      );
-
-      if (!ChatBot.session_id) {
-        alert("Session ID is missing, please start a new chat.");
-        return;
-      }
       dispatch(
         fetchSendMessage({
           message: data.message,
-          session_id: ChatBot.session_id,
         })
       );
     } catch (error) {
