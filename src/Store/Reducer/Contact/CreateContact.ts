875dd2005c1bc574e9d1dd18421f5ec214db305c
moshaosama <mohamedOSFekry@gmail.com>
2025-05-17 19:07:55 +0300

    Update CreateContact.ts

--- src/Store/Reducer/Contact/CreateContact.ts
+++ src/Store/Reducer/Contact/CreateContact.ts
@@ -13,8 +13,7 @@ export const fetchCreateContact = createAsyncThunk(
   async (data: { Name: string; Email: string; Comment: string }) => {
     const Token = window.localStorage.getItem("Token");
     const response = await axios.post(
-      // "https://graduationprojectserver-production.up.railway.app/createcontact",
-      "http://localhost:3000/createcontact",
+      "https://graduationprojectserver-production.up.railway.app/createcontact",
       {
         Name: data.Name,
         Email: data.Email,
