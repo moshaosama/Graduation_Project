fd7614a32ea41429895a9166693a8ace4101746d
moshaosama <mohamedOSFekry@gmail.com>
2025-05-17 19:07:56 +0300

    Update CreateNewMedicine.ts

--- src/Store/Reducer/MedicineTracker/CreateNewMedicine.ts
+++ src/Store/Reducer/MedicineTracker/CreateNewMedicine.ts
@@ -20,8 +20,7 @@ export const createNewMedicine = createAsyncThunk(
   async ({ Name, Description }: DataMedicine) => {
     try {
       const response = await axios.post(
-        // "https://graduationprojectserver-production.up.railway.app/create_new_medicine",
-        "http://localhost:3000/create_new_medicine",
+        "https://graduationprojectserver-production.up.railway.app/create_new_medicine",
         {
           userId: user?.id,
           Name,
