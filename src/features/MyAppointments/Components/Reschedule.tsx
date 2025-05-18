24ae619a1320b57d9f38e891cc8307ade363b4bf
moshaosama <mohamedOSFekry@gmail.com>
2025-05-18 22:33:28 +0300

    Update Reschedule.tsx

--- src/features/MyAppointments/Components/Reschedule.tsx
+++ src/features/MyAppointments/Components/Reschedule.tsx
@@ -1,13 +1,18 @@
 import { useOpenRescheduleContext } from "../Context/OpenReschedule";
+import useFormData from "../Hooks/useFormData";
 import useGetAppointmentByID from "../Hooks/useGetAppointmentByID";
 const Reschedule = () => {
   const { handleCloseReschedule } = useOpenRescheduleContext();
   const { AppointmentyById } = useGetAppointmentByID();
+  const { register, handleSubmit, errors, onSubmit } = useFormData();
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center z-50">
       <div className="bg-white p-4 rounded-lg shadow-lg shadow-black  w-[40pc]">
         <h1 className="text-2xl font-bold">Reschedule Appointment</h1>
-        <form className="grid grid-cols-2 gap-4 mt-10">
+        <form
+          className="grid grid-cols-2 gap-4 mt-10"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <p className="col-span-2 flex flex-col gap-2">
             <label
               htmlFor="userName"
@@ -17,9 +22,13 @@ const Reschedule = () => {
             </label>
             <input
               type="text"
-              placeholder={AppointmentyById.data.result[0].Name}
+              placeholder={AppointmentyById.data.result?.[0].Name}
               className="p-2 rounded-lg border-2 border-gray-400"
+              {...register("Name")}
             />
+            {errors.Name && (
+              <p className="text-red-500">{errors.Name.message as string}</p>
+            )}
           </p>
           <p className="col-span-2 flex flex-col gap-2">
             <label
@@ -31,9 +40,17 @@ const Reschedule = () => {
             <input
               type="number"
               inputMode="numeric"
-              placeholder={String(AppointmentyById.data.result[0].PhoneNumber)}
+              placeholder={String(
+                AppointmentyById.data.result?.[0].PhoneNumber
+              )}
               className="p-2 rounded-lg border-2 border-gray-400"
+              {...register("PhoneNumber")}
             />
+            {errors.PhoneNumber && (
+              <p className="text-red-500">
+                {errors.PhoneNumber.message as string}
+              </p>
+            )}
           </p>
           <p className="col-span-2 flex flex-col gap-2">
             <label htmlFor="email" className="text-lg font-bold text-gray-500">
@@ -41,9 +58,13 @@ const Reschedule = () => {
             </label>
             <input
               type="email"
-              placeholder={AppointmentyById.data.result[0].Email}
+              placeholder={AppointmentyById.data.result?.[0].Email}
               className="p-2 rounded-lg border-2 border-gray-400"
+              {...register("Email")}
             />
+            {errors.Email && (
+              <p className="text-red-500">{errors.Email.message as string}</p>
+            )}
           </p>
           <p className="col-span-2 flex justify-end gap-2">
             <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
