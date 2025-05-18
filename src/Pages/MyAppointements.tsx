090d9dd6ef1f1dfec87eaadbcda296b413470a91
moshaosama <mohamedOSFekry@gmail.com>
2025-05-18 21:58:28 +0300

    Update MyAppointements.tsx

--- src/Pages/MyAppointements.tsx
+++ src/Pages/MyAppointements.tsx
@@ -2,13 +2,16 @@ import { Bounce, ToastContainer } from "react-toastify";
 import Faq from "../features/MyAppointments/Components/Faq";
 import GetAppointments from "../features/MyAppointments/Components/getAppointments";
 import Header from "../features/MyAppointments/Components/Header";
-
+import Reschedule from "../features/MyAppointments/Components/Reschedule";
+import { useOpenRescheduleContext } from "../features/MyAppointments/Context/OpenReschedule";
 const MyAppointements = () => {
+  const { OpenReschedule } = useOpenRescheduleContext();
   return (
     <>
       <Header />
       <GetAppointments />
       <Faq />
+      {OpenReschedule && <Reschedule />}
       <ToastContainer
         position="top-right"
         autoClose={5000}
