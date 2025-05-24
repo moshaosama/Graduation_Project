d2cdad6311f95c83a168ce0917f2278b721a84fc
moshaosama <mohamedOSFekry@gmail.com>
2025-05-24 20:42:57 +0300

    Commit EndChatBot.tsx changes

--- src/Components/EndChatBot.tsx
+++ src/Components/EndChatBot.tsx
@@ -1,7 +1,10 @@
 import { useCloseChatBotContext } from "../Context/CloseChatBot";
+import { useOpenChatApi } from "../features/PopUpChatBot/Context/useOpenChatApi";
 
 const EndChatBot = () => {
   const { CloseChat } = useCloseChatBotContext();
+  const { handleCloseChat } = useOpenChatApi();
+
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 backdrop-blur-sm z-50 flex justify-center items-center">
       <div className="bg-white shadow-black shadow-lg p-5 rounded-xl">
@@ -9,11 +12,17 @@ const EndChatBot = () => {
           Are you sure you want to close the chat?
         </h1>
         <div className="flex justify-around items-center mt-5">
-          <button className="bg-blue-200 px-6   max-sm:w-fit max-sm:px-4 py-3  rounded-xl font-bold hover:bg-blue-300 transition-all duration-300 max-sm:py-1">
+          <button
+            className="bg-blue-200 px-6 max-sm:w-fit max-sm:px-4 py-3 rounded-xl font-bold hover:bg-blue-300 transition-all duration-300 max-sm:py-1"
+            onClick={() => {
+              handleCloseChat();
+              CloseChat();
+            }}
+          >
             Yes
           </button>
           <button
-            className="bg-blue-200 px-6  max-sm:w-fit max-sm:px-4 py-3  rounded-xl font-bold hover:bg-blue-300 transition-all duration-300 max-sm:py-1"
+            className="bg-blue-200 px-6 max-sm:w-fit max-sm:px-4 py-3 rounded-xl font-bold hover:bg-blue-300 transition-all duration-300 max-sm:py-1"
             onClick={CloseChat}
           >
             No
