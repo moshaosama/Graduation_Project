6ec1af7b6ad2a1dc391dbc86295c784a2cd5ec62
moshaosama <mohamedOSFekry@gmail.com>
2025-05-24 20:42:57 +0300

    Commit CloseChatBot.tsx changes

--- src/Context/CloseChatBot.tsx
+++ src/Context/CloseChatBot.tsx
@@ -1,27 +1,56 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { AppDispatch } from "../Store/Store";
+import { useDispatch } from "react-redux";
+import { fetchCreateHistory } from "../features/ChatBot/Action/CreateHistory";
 
 interface CloseEndChatType {
   isCloseChat: boolean;
   OpenCloseChat: () => void;
   CloseChat: () => void;
   handleTriggerCloseChat: () => void;
+  handleCloseChatWithCreateHoistory: (chatData: any[]) => void;
+  ChatMessage: any;
 }
 
 const CloseChatBotContext = createContext<CloseEndChatType | null>(null);
 
 export const CloseChatBotProvider = ({ children }: { children: ReactNode }) => {
   const [isCloseChat, setIsCloseChat] = useState(false);
+  const [ChatMessage, setChatMessage] = useState<any>([]);
+  const dispatch = useDispatch<AppDispatch>();
+  const User = JSON.parse(window.localStorage.getItem("User")!);
+
+  useEffect(() => {
+    console.log("ChatMessage updated:", ChatMessage);
+  }, [ChatMessage]);
 
   const OpenCloseChat = () => {
     setIsCloseChat(true);
   };
 
-  const CloseChat = () => {
+  const CloseChat = async () => {
     setIsCloseChat(false);
+    await dispatch(
+      fetchCreateHistory({
+        Data: JSON.stringify(ChatMessage),
+        userId: User?.id,
+      })
+    );
   };
 
   const handleTriggerCloseChat = () => {
     setIsCloseChat(!isCloseChat);
+  };
+
+  const handleCloseChatWithCreateHoistory = async (chatData: any[]) => {
+    setChatMessage(chatData);
+    console.log(chatData);
   };
 
   return (
@@ -32,6 +61,8 @@ export const CloseChatBotProvider = ({ children }: { children: ReactNode }) => {
           OpenCloseChat,
           CloseChat,
           handleTriggerCloseChat,
+          handleCloseChatWithCreateHoistory,
+          ChatMessage,
         }}
       >
         {children}
