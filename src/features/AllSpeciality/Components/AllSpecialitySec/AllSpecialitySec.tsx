25c2136133ebcb279ab9dc873d36872ad8f93ebf
moshaosama <mohamedOSFekry@gmail.com>
2025-06-02 01:53:40 +0300

    Update AllSpecialitySec component

--- src/features/AllSpeciality/Components/AllSpecialitySec/AllSpecialitySec.tsx
+++ src/features/AllSpeciality/Components/AllSpecialitySec/AllSpecialitySec.tsx
@@ -1,14 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../../../Store/Store";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { fetchSpeciality } from "../../../../Store/Reducer/Speciality/SpecialityReducer";
 import { fetchDoctor } from "../../../../Store/Reducer/Doctor/DoctorReducer";
 import { useNavigate } from "react-router";
+import useTransition from "../../../../Hooks/useTransition";
+
+const initialStyle = {
+  gap: "40px",
+};
+
+const Styles = {
+  gap: "17px",
+};
 
 const AllSpecialitySec = () => {
   const state = useSelector((state: RootState) => state.Speciality);
   const Navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
+  const AllSpeciality = useRef(null);
 
   useEffect(() => {
     dispatch(fetchSpeciality());
@@ -19,20 +29,25 @@ const AllSpecialitySec = () => {
     Navigate("/doctors");
   };
 
+  useTransition(AllSpeciality, initialStyle, Styles);
+
   return (
     <>
-      <div className={`bg-white mt-12 w-full `}>
-        <div className="mx-80 max-sm:mx-7 py-5">
+      <div className={`mt-12 w-full`}>
+        <div className="py-5 mx-80 max-sm:mx-7">
           <h1 className="text-2xl font-bold text-[#343437]">
             Select a specialty
           </h1>
-          <div className="grid grid-cols-2 gap-5 mt-5">
+          <div
+            ref={AllSpeciality}
+            className="grid grid-cols-2 gap-5 mt-5 gap-x-[0pc] transition-all duration-1000"
+          >
             {state.data.result?.map(
               (el: { Specialty_name: string }, index: number) => {
                 return (
                   <h1
                     key={index}
-                    className="text-blue-700 font-bold text-lg hover:underline cursor-pointer"
+                    className="text-lg font-bold text-blue-700 cursor-pointer hover:underline"
                     onClick={() =>
                       handleClick({ Speciality: el.Specialty_name })
                     }
