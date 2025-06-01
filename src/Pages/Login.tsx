97e61f7b3418fc308ec9349bf2936900577577a8
moshaosama <mohamedOSFekry@gmail.com>
2025-06-02 01:53:40 +0300

    Update Login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -4,23 +4,28 @@ import FormLogin from "../features/Login/Components/FormLogin/FormLogin";
 import clsk from "clsx";
 import useOpenWindow from "../Hooks/useOpenWindow";
 
+const initalStyle = {
+  opacity: "0",
+  transform: "scale(1.5)",
+};
+
 const Login = () => {
   const { Open } = useOpenWindow();
 
   return (
     <>
-      <div className="flex justify-center items-center h-screen bg-[#eee]">
+      <div className="flex justify-center opacity-100 transition-all duration-1000 scale-100 items-center h-screen bg-[#eee]">
         <div
           className={clsk(
             "rounded-xl bg-white w-96 p-10 relative transition-all duration-500",
             Open ? "opacity-100 scale-100" : "opacity-0 scale-50"
           )}
         >
-          <div className="bg-blue-500 w-full rounded-tr-lg rounded-tl-lg absolute top-0 left-0">
-            <h1 className="text-center text-white font-bold p-1">login</h1>
+          <div className="absolute top-0 left-0 w-full bg-blue-500 rounded-tl-lg rounded-tr-lg">
+            <h1 className="p-1 font-bold text-center text-white">login</h1>
           </div>
           <FormLogin />
-          <div className="w-full text-end my-2 hover:underline">
+          <div className="w-full my-2 text-end hover:underline">
             <Link to={""} className="text-blue-500">
               Forget your password?
             </Link>
